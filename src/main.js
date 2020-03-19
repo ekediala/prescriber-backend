aa593b348e7eeb1305705dbad203a40c30ba29b9
Eke Enyinnaya Diala <devenyinna@Ekes-MacBook-Pro.local>
2020-03-19 22:42:51 +0100

    --amend

--- src/main.js
+++ src/main.js
@@ -16,13 +16,6 @@ const app = express();
 app.use(express.json());
 app.use(cacheControl());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-
 app.use(cors());
 
 app.use("/v1", router);
